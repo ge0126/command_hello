a8498992bf91a7ff79e893768025038557a76872
ge0126 <32252098+ge0126@users.noreply.github.com>
2017-09-27 10:07:33 +0900

    directly modified on Github

--- test.c
+++ test.c
@@ -4,6 +4,7 @@ int main()
 {
 	printf("Hello Git World\n");
 	printf("Tell your world\n");
-	printf("Tell my  world\n");
+	printf("Tell my world\n\n");
+	printf("Tell gaeun world\n");
 	return 0;
 }
